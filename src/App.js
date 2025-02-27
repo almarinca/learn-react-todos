d6e0d2be2ae226e2c0f9d38ffce6ef86f0735309
Alejandro Marin <almarinca@unal.edu.co>
2025-02-27 10:55:04 -0500

    create first react element, a to-do item

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+
     </div>
   );
+}
+
+function TodoItem() {
+  return (
+    <li>
+      <span>✅</span>
+      <span>Finish To-do's app</span>
+      <span>❌</span>
+    </li>
+  )
 }
 
 export default App;
