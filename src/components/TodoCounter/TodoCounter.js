5e9ccf96fb7cc7e2faaa677f863dd5b1352e0afd
Alejandro Marin <almarinca@unal.edu.co>
2025-03-07 15:57:52 -0500

    Message for all task completed

--- src/components/TodoCounter/TodoCounter.js
+++ src/components/TodoCounter/TodoCounter.js
@@ -1,8 +1,11 @@
 import './TodoCounter.css'
 
 function TodoCounter({total, completed}) {
+    const completedMsg = "Congratulations! You have completed all your tasks"
     return (
-        <h1 className='todo-heading'>You have completed {completed} of {total} tasks</h1>
+        <h1 className='todo-heading'>
+            {(completed === total) ? completedMsg : `You have completed ${completed} of ${total} tasks`}
+        </h1>
     )
 }
 
